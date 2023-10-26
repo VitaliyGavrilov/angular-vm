304e960621c88fc61dbd8d03ad00265ffaebfc43
VitaliyGavrilov <vitaliy140798gavrilov@mail.ru>
2023-10-27 00:49:41 +0300

    +задержка стрима на 2сек

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core'
 import {HttpClient, HttpParams} from '@angular/common/http'
-import { Observable } from 'rxjs'
+import { Observable, delay } from 'rxjs'
 import { IProduct } from '../models/product'
 
 @Injectable({
@@ -14,6 +14,8 @@ export class ProductsService {
   getAll(): Observable<IProduct[]> {
     return this.http.get<IProduct[]>('https://fakestoreapi.com/products', {
       params: new HttpParams().append('limit', 5)
-    })
+    }).pipe(
+      delay(2000)
+    )
   }
 }
